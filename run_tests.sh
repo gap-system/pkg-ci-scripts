5f3ed6fe2c9623838d878f626567c17b76e2e321
Max Horn <max@quendi.de>
2019-02-23 11:00:58 +0100

    Make run_tests.sh reentrant safe

--- run_tests.sh
+++ run_tests.sh
@@ -22,7 +22,7 @@ GAP="$GAPROOT/bin/gap.sh -l $PWD/gaproot; --quitonbreak"
 # Unless explicitly turned off by setting the NO_COVERAGE environment variable,
 # we collect coverage data
 if [[ -z $NO_COVERAGE ]]; then
-    mkdir $COVDIR
+    mkdir -p $COVDIR
     GAP="$GAP --cover $COVDIR/$(mktemp XXXXXX).coverage"
 fi
 
